749f79ca5c8c9a5fcede4c60b8308e5ace52c913
beautyke <1598461649@qq.com>
2020-02-28 23:26:01 +0800

    理解逻辑关系

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,15 +1,13 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+  <div>Home</div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'Home'
 }
 </script>
 
-<style>
+<style scoped>
 
 </style>
