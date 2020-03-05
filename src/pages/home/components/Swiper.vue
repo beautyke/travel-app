7b0d7df90b6d7d7a070fc06e1dc4319e0036a7a7
beautyke <1598461649@qq.com>
2020-03-05 22:43:31 +0800

    'swiper'

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="wrapper">
+        <swiper :options="swiperOption">
+            <swiper-slide>
+                <img class="swiper-img" src="../../../../src/assets/images/swiper-01.jpg"/>
+            </swiper-slide>
+            <swiper-slide>
+                <img class="swiper-img" src="../../../../src/assets/images/swiper-02.jpg"/>
+            </swiper-slide>
+            <swiper-slide>
+                <img class="swiper-img" src="../../../../src/assets/images/swiper-03.jpg"/>
+            </swiper-slide>
+            <div class="swiper-pagination"  slot="pagination"></div>
+        </swiper>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        // 循环轮播
+        loop: true
+      }
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: #fff
+  .wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 31.25%
+    background: #eee
+    .swiper-img
+        width: 100%
+</style>
