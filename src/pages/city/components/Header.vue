8a9836683b702de32c2b11def0312955f25aa8d6
beautyke <1598461649@qq.com>
2020-03-11 21:08:01 +0800

    city路由配置

---
+++ src/pages/city/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="header">
+    城市选择
+    <router-link to="/">
+      <div class="iconfont header-back">&#xe624;</div>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CityHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .header
+    position: relative
+    overflow: hidden
+    height: $headerHeight
+    line-height: $headerHeight
+    text-align: center
+    color: #fff
+    background: $bgColor
+    .header-back
+      position: absolute
+      top: 0
+      left: 0
+      widows 0.64rem
+      text-align: center
+      font-size: .4rem
+      color: #fff
+</style>
--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -2,7 +2,9 @@
   <div class="header">
      <div class="header-left"><div class="iconfont back-icon">&#xe624;</div></div>
      <div class="header-input"><span class="iconfont">&#xe632;</span>输入城市/景点/游玩主题</div>
-     <div class="header-right">{{this.city}}<span class="iconfont arrow-icon">&#xe600;</span></div>
+     <router-link to='/city'>
+       <div class="header-right">{{this.city}}<span class="iconfont arrow-icon">&#xe600;</span></div>
+     </router-link>
   </div>
 </template>
 
@@ -19,7 +21,7 @@ export default {
   @import '~styles/varibles.styl'
   .header
     display: flex
-    line-height: .86rem
+    line-height: $headerHeight
     background: $bgColor
     color: #fff
     .header-left
@@ -42,6 +44,7 @@ export default {
       width: 1.24rem
       float: right
       text-align: center
+      color: #fff
       .arrow-icon
         margin-left: .2rem
         font-size: .24rem
