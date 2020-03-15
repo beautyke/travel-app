d74cf4c706102ea65954ce2951af70e4ece1e13a
beautyke <1598461649@qq.com>
2020-03-15 17:21:02 +0800

    keep-alive

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -15,6 +15,7 @@ import HomeIcons from './components/Icons'
 import HomeRecommend from './components/Recommend'
 import HomeWeekend from './components/Weekend'
 import axios from 'axios'
+import { mapState } from 'vuex'
 
 export default {
   name: 'Home',
@@ -30,12 +31,16 @@ export default {
       swiperList: [],
       iconsList: [],
       recommendList: [],
-      weekendList: []
+      weekendList: [],
+      lastCity: ''
     }
+  },
+  computed: {
+    ...mapState(['city'])
   },
   methods: {
     getHomeInfo () {
-      axios.get('/api/index.json')
+      axios.get('/api/index.json?city=' + this.city)
         .then(this.getHomeInfoSucc)
     },
     getHomeInfoSucc (res) {
@@ -50,7 +55,16 @@ export default {
     }
   },
   mounted () {
+    // 当页面被挂载之后，我们对上一次的城市进行保存
+    this.lastCity = this.city
     this.getHomeInfo()
+  },
+  activated () {
+    // 当页面进行重新挂载的时候,我们就去重新发一次ajax请求
+    if (this.lastCity !== this.city) {
+      this.lastCity = this.city
+      this.getHomeInfo()
+    }
   }
 }
 </script>
