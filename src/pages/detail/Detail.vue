5c38c2793f05c969984541caa4d539f5c09553a5
beautyke <1598461649@qq.com>
2020-03-16 00:06:00 +0800

    递归组件使用

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -2,19 +2,46 @@
   <div>
     <detail-banner></detail-banner>
     <detail-header></detail-header>
-    <div class="content"></div>
+    <div class="content">
+      <detail-list :list="list"></detail-list>
+    </div>
   </div>
 </template>
 
 <script>
 import DetailBanner from './compoents/Banner'
 import DetailHeader from './compoents/Header'
+import DetailList from './compoents/List'
 
 export default {
   name: 'Detail',
   components: {
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailList
+  },
+  data () {
+    return {
+      list: [{
+        title: '成人票',
+        children: [{
+          title: '成人三馆联票',
+          children: [{
+            title: '成人三馆联票--仓山区店'
+          }]
+        }, {
+          title: '成人五馆联票'
+        }, {
+          title: '成人六馆联票'
+        }]
+      }, {
+        title: '学生票'
+      }, {
+        title: '儿童票'
+      }, {
+        title: '特惠票'
+      }]
+    }
   }
 }
 </script>
